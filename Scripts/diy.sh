9551c305881f2104e1ba7b4e07bcae461aaee46e
zongheng23333 <1954640276@qq.com>
2024-07-19 15:43:55 +0800

    Create diy.sh

---
+++ Scripts/diy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+./scripts/feeds update -a 
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+
+rm -rf feeds/luci/applications/luci-app-openclash
+cp -rf feeds/smpackage/luci-app-openclash   feeds/luci/applications/luci-app-openclash
+git clone https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
+./scripts/feeds install -a  
