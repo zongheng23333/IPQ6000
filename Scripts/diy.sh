a7a0d6ba6297f0b6f2b7eca98ef78b238e73ab89
zongheng23333 <1954640276@qq.com>
2024-08-03 20:29:07 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
  sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+
+ sed -i '$a src-git ikoolproxy   https://github.com/ilxp/luci-app-ikoolproxy' feeds.conf.default
 ./scripts/feeds update -a 
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 
